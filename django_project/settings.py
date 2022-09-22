d0575814e0d6740b1b49ae27562729a4c3f7c796
basheerrjoub <basheer20599@gmail.com>
2022-09-23 00:18:16 +0300

    added Proc and runtime for production

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-2p53hx3qsxjvxtju%#*@5ib#i^jpo)fuf7dp-9+)dzw-nho7)#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
